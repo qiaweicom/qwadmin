4a18abe114fc3954a01fdd960318637ebea0a887
寒川 <misscx@msn.cn>
2016-01-29 11:39:42 +0800

    新增部分菜单

--- sql.sql
+++ sql.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2016 年 01 月 28 日 14:04
+-- 生成日期: 2016 年 01 月 28 日 12:56
 -- 服务器版本: 5.1.63
 -- PHP 版本: 5.2.17p1
 
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `qw_auth_group` (
 --
 
 INSERT INTO `qw_auth_group` (`id`, `title`, `status`, `rules`) VALUES
-(1, '超级管理员', 1, '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,29,30,31,32,33,34,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55');
+(1, '超级管理员', 1, '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,29,30,31,32,33,34,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57');
 
 -- --------------------------------------------------------
 
@@ -101,66 +101,68 @@ CREATE TABLE IF NOT EXISTS `qw_auth_rule` (
   `o` int(11) NOT NULL COMMENT '排序',
   `tips` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=56 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=58 ;
 
 --
 -- 转存表中的数据 `qw_auth_rule`
 --
 
 INSERT INTO `qw_auth_rule` (`id`, `pid`, `name`, `title`, `icon`, `type`, `status`, `condition`, `islink`, `o`, `tips`) VALUES
-(1, 0, 'Index/index', '控制台', 'menu-icon fa fa-tachometer', 1, 1, '', 1, 0, '友情提示：经常查看操作日志，发现异常以便及时追查原因。'),
-(2, 0, '', '系统设置', 'menu-icon fa fa-cog', 1, 1, '', 1, 0, ''),
-(3, 2, 'Setting/setting', '网站设置', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, '这是网站设置的提示。'),
-(4, 2, 'Menu/index', '后台菜单', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(5, 2, 'Menu/add', '新增菜单', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(6, 4, 'Menu/edit', '编辑菜单', '', 1, 1, '', 0, 0, ''),
-(7, 2, 'Menu/update', '保存菜单', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 0, ''),
-(8, 2, 'Menu/del', '删除菜单', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 0, ''),
-(9, 2, 'Database/backup', '数据库备分', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(10, 2, 'Database/recovery', '数据库还原', '', 1, 1, '', 0, 0, ''),
-(11, 2, 'Update/update', '在线升级', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(12, 2, 'Update/devlog', '开发日志', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(13, 0, '', '用户及组', 'menu-icon fa fa-users', 1, 1, '', 1, 0, ''),
-(14, 13, 'Member/index', '用户管理', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(15, 13, 'Member/add', '新增用户', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(16, 13, 'Member/edit', '编辑用户', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 0, ''),
-(17, 13, 'Member/update', '保存用户', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 0, ''),
-(18, 13, 'Member/del', '删除用户', '', 1, 1, '', 0, 0, ''),
-(19, 13, 'Group/index', '用户组管理', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(20, 13, 'Group/add', '新增用户组', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(21, 13, 'Group/edit', '编辑用户组', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 0, ''),
-(22, 13, 'Group/update', '保存用户组', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 0, ''),
-(23, 13, 'Group/edit', '删除用户组', '', 1, 1, '', 0, 0, ''),
-(24, 0, '', '网站内容', 'menu-icon fa fa-desktop', 1, 1, '', 1, 0, ''),
-(25, 24, 'Article/index', '文章管理', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, '网站虽然重要，身体更重要，出去走走吧。'),
-(26, 24, 'Article/add', '新增文章', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(27, 24, 'Article/edit', '编辑文章', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 0, ''),
-(29, 24, 'Article/update', '保存文章', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 0, ''),
-(30, 24, 'Article/del', '删除文章', '', 1, 1, '', 0, 0, ''),
-(31, 24, 'Category/index', '分类管理', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(32, 24, 'Category/add', '新增分类', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(33, 24, 'Category/edit', '编辑分类', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 0, ''),
-(34, 24, 'Category/update', '保存分类', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 0, ''),
-(36, 24, 'Category/del', '删除分类', '', 1, 1, '', 0, 0, ''),
-(37, 0, '', '其它功能', 'menu-icon fa fa-legal', 1, 1, '', 1, 0, ''),
-(38, 37, 'Link/index', '友情链接', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 0, ''),
-(39, 37, 'Link/add', '增加链接', '', 1, 1, '', 1, 0, ''),
-(40, 37, 'Link/edit', '编辑链接', '', 1, 1, '', 0, 0, ''),
-(41, 37, 'Link/update', '保存链接', '', 1, 1, '', 0, 0, ''),
-(42, 37, 'Link/del', '删除链接', '', 1, 1, '', 0, 0, ''),
-(43, 37, 'Flash/index', '焦点图', 'menu-icon fa fa-desktop', 1, 1, '', 1, 0, ''),
-(44, 37, 'Flash/add', '新增焦点图', '', 1, 1, '', 1, 0, ''),
-(45, 37, 'Flash/update', '保存焦点图', '', 1, 1, '', 0, 0, ''),
-(46, 37, 'Flash/edit', '编辑焦点图', '', 1, 1, '', 0, 0, ''),
-(47, 37, 'Flash/del', '删除焦点图', '', 1, 1, '', 0, 0, ''),
-(48, 0, 'Personal/index', '个人中心', 'menu-icon fa fa-user', 1, 1, '', 1, 0, ''),
-(49, 48, 'Personal/profile', '个人资料', 'menu-icon fa fa-user', 1, 1, '', 1, 0, ''),
-(50, 48, 'Logout/index', '退出', '', 1, 1, '', 1, 0, ''),
-(51, 9, 'Database/export', '备分', '', 1, 1, '', 0, 0, ''),
-(52, 9, 'Database/optimize', '数据优化', '', 1, 1, '', 0, 0, ''),
-(53, 9, 'Database/repair', '修复表', '', 1, 1, '', 0, 0, ''),
-(54, 11, 'Update/updating', '升级安装', '', 1, 1, '', 0, 0, ''),
-(55, 48, 'Personal/update', '资料保存', '', 1, 1, '', 0, 0, '');
+(1, 0, 'Index/index', '控制台', 'menu-icon fa fa-tachometer', 1, 1, '', 1, 1, '友情提示：经常查看操作日志，发现异常以便及时追查原因。'),
+(2, 0, '', '系统设置', 'menu-icon fa fa-cog', 1, 1, '', 1, 2, ''),
+(3, 2, 'Setting/setting', '网站设置', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 3, '这是网站设置的提示。'),
+(4, 2, 'Menu/index', '后台菜单', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 4, ''),
+(5, 2, 'Menu/add', '新增菜单', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 5, ''),
+(6, 4, 'Menu/edit', '编辑菜单', '', 1, 1, '', 0, 6, ''),
+(7, 2, 'Menu/update', '保存菜单', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 7, ''),
+(8, 2, 'Menu/del', '删除菜单', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 8, ''),
+(9, 2, 'Database/backup', '数据库备分', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 9, ''),
+(10, 9, 'Database/recovery', '数据库还原', '', 1, 1, '', 0, 10, ''),
+(11, 2, 'Update/update', '在线升级', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 11, ''),
+(12, 2, 'Update/devlog', '开发日志', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 12, ''),
+(13, 0, '', '用户及组', 'menu-icon fa fa-users', 1, 1, '', 1, 13, ''),
+(14, 13, 'Member/index', '用户管理', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 14, ''),
+(15, 13, 'Member/add', '新增用户', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 15, ''),
+(16, 13, 'Member/edit', '编辑用户', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 16, ''),
+(17, 13, 'Member/update', '保存用户', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 17, ''),
+(18, 13, 'Member/del', '删除用户', '', 1, 1, '', 0, 18, ''),
+(19, 13, 'Group/index', '用户组管理', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 19, ''),
+(20, 13, 'Group/add', '新增用户组', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 20, ''),
+(21, 13, 'Group/edit', '编辑用户组', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 21, ''),
+(22, 13, 'Group/update', '保存用户组', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 22, ''),
+(23, 13, 'Group/del', '删除用户组', '', 1, 1, '', 0, 23, ''),
+(24, 0, '', '网站内容', 'menu-icon fa fa-desktop', 1, 1, '', 1, 24, ''),
+(25, 24, 'Article/index', '文章管理', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 25, '网站虽然重要，身体更重要，出去走走吧。'),
+(26, 24, 'Article/add', '新增文章', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 26, ''),
+(27, 24, 'Article/edit', '编辑文章', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 27, ''),
+(29, 24, 'Article/update', '保存文章', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 29, ''),
+(30, 24, 'Article/del', '删除文章', '', 1, 1, '', 0, 30, ''),
+(31, 24, 'Category/index', '分类管理', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 31, ''),
+(32, 24, 'Category/add', '新增分类', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 32, ''),
+(33, 24, 'Category/edit', '编辑分类', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 33, ''),
+(34, 24, 'Category/update', '保存分类', 'menu-icon fa fa-caret-right', 1, 1, '', 0, 34, ''),
+(36, 24, 'Category/del', '删除分类', '', 1, 1, '', 0, 36, ''),
+(37, 0, '', '其它功能', 'menu-icon fa fa-legal', 1, 1, '', 1, 37, ''),
+(38, 37, 'Link/index', '友情链接', 'menu-icon fa fa-caret-right', 1, 1, '', 1, 38, ''),
+(39, 37, 'Link/add', '增加链接', '', 1, 1, '', 1, 39, ''),
+(40, 37, 'Link/edit', '编辑链接', '', 1, 1, '', 0, 40, ''),
+(41, 37, 'Link/update', '保存链接', '', 1, 1, '', 0, 41, ''),
+(42, 37, 'Link/del', '删除链接', '', 1, 1, '', 0, 42, ''),
+(43, 37, 'Flash/index', '焦点图', 'menu-icon fa fa-desktop', 1, 1, '', 1, 43, ''),
+(44, 37, 'Flash/add', '新增焦点图', '', 1, 1, '', 1, 44, ''),
+(45, 37, 'Flash/update', '保存焦点图', '', 1, 1, '', 0, 45, ''),
+(46, 37, 'Flash/edit', '编辑焦点图', '', 1, 1, '', 0, 46, ''),
+(47, 37, 'Flash/del', '删除焦点图', '', 1, 1, '', 0, 47, ''),
+(48, 0, 'Personal/index', '个人中心', 'menu-icon fa fa-user', 1, 1, '', 1, 48, ''),
+(49, 48, 'Personal/profile', '个人资料', 'menu-icon fa fa-user', 1, 1, '', 1, 49, ''),
+(50, 48, 'Logout/index', '退出', '', 1, 1, '', 1, 50, ''),
+(51, 9, 'Database/export', '备分', '', 1, 1, '', 0, 51, ''),
+(52, 9, 'Database/optimize', '数据优化', '', 1, 1, '', 0, 52, ''),
+(53, 9, 'Database/repair', '修复表', '', 1, 1, '', 0, 53, ''),
+(54, 11, 'Update/updating', '升级安装', '', 1, 1, '', 0, 54, ''),
+(55, 48, 'Personal/update', '资料保存', '', 1, 1, '', 0, 55, ''),
+(56, 3, 'Setting/update', '设置保存', '', 1, 1, '', 0, 56, ''),
+(57, 0, 'Database/del', '备份删除', '', 1, 1, '', 0, 57, '');
 
 -- --------------------------------------------------------
 
@@ -177,8 +179,7 @@ CREATE TABLE IF NOT EXISTS `qw_category` (
   `o` int(11) NOT NULL COMMENT '排序',
   PRIMARY KEY (`id`),
   KEY `fsid` (`pid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 -- --------------------------------------------------------
 
@@ -282,7 +283,7 @@ CREATE TABLE IF NOT EXISTS `qw_member` (
   `password` varchar(32) NOT NULL,
   `t` int(10) unsigned NOT NULL COMMENT '注册时间',
   PRIMARY KEY (`uid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2;
 
 --
 -- 转存表中的数据 `qw_member`
@@ -312,4 +313,4 @@ INSERT INTO `qw_setting` (`k`, `v`) VALUES
 ('title', 'QWADMIN'),
 ('keywords', '关键词一,关键词二'),
 ('description', '网站描述'),
-('footer', '2016©恰维网络');
+('footer', '2016&copy;恰维网络');
