0b2cb8aff64971c4501d79cced1e076cdfe4c43f
kang <41837647@qq.com>
2016-02-20 12:35:59 +0800

    修复flash表数据库无法提交保存的BUG

--- sql.sql
+++ sql.sql
@@ -238,7 +238,6 @@ CREATE TABLE IF NOT EXISTS `qw_flash` (
   `url` varchar(255) NOT NULL,
   `pic` varchar(255) NOT NULL,
   `o` int(11) NOT NULL,
-  `s` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `o` (`o`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
