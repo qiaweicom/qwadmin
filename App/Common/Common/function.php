c021f021478ff62f972d9a151a414dd2aae06fd2
寒川 <misscx@msn.cn>
2016-04-23 22:54:20 +0800

    更改cookie认证方式

--- App/Common/Common/function.php
+++ App/Common/Common/function.php
@@ -63,3 +63,32 @@ function password($password){
 	return md5('Q'.$password.'W');
 
 }
+
+/**
+ * 随机字符
+ * @param number $length 长度
+ * @param string $type 类型
+ * @param number $convert 转换大小写
+ * @return string
+ */
+function random($length=6, $type='string', $convert=0){
+    $config = array(
+        'number'=>'1234567890',
+        'letter'=>'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        'string'=>'abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789',
+        'all'=>'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    );
+    
+    if(!isset($config[$type])) $type = 'string';
+    $string = $config[$type];
+    
+    $code = '';
+    $strlen = strlen($string) -1;
+    for($i = 0; $i < $length; $i++){
+        $code .= $string{mt_rand(0, $strlen)};
+    }
+    if(!empty($convert)){
+        $code = ($convert > 0)? strtoupper($code) : strtolower($code);
+    }
+    return $code;
+}
