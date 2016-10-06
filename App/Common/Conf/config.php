272a0a2b355aa598e3e749f417d52d4cce1f8c55
kang <41837647@qq.com>
2016-10-06 20:07:07 +0800

    增加config.php配置文件

---
+++ App/Common/Conf/config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * 版权所有：恰维网络<qwadmin.qiawei.com>
+ * 作    者：寒川<hanchuan@qiawei.com>
+ * 日    期：2015-09-15
+ * 版    本：1.0.0
+ * 功能说明：配置文件。
+ *
+ **/
+return array(
+    //网站配置信息
+    'URL' => 'http://www.qwadmin.com', //网站根URL
+    'COOKIE_SALT' => '', //设置cookie加密密钥
+    //备份配置
+    'DB_PATH_NAME' => 'db',        //备份目录名称,主要是为了创建备份目录
+    'DB_PATH' => './db/',     //数据库备份路径必须以 / 结尾；
+    'DB_PART' => '20971520',  //该值用于限制压缩后的分卷最大长度。单位：B；建议设置20M
+    'DB_COMPRESS' => '1',         //压缩备份文件需要PHP环境支持gzopen,gzwrite函数        0:不压缩 1:启用压缩
+    'DB_LEVEL' => '9',         //压缩级别   1:普通   4:一般   9:最高
+    //扩展配置文件
+    'LOAD_EXT_CONFIG' => 'db',
+);
