7c532d813888873f2d433e99bb79d45ef4161311
kang <41837647@qq.com>
2016-10-06 19:57:26 +0800

    qwadmin相关配置信息整理

---
+++ App/Common/Conf/db.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * 版权所有：恰维网络<www.qiawei.com>
+ * 作    者：kang<41837647@qq.com>
+ * 日    期：2016-10-06
+ * 版    本：1.2.0
+ * 功能说明：数据库配置文件。
+ *
+ **/
+return array(
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => 'localhost', // 服务器地址
+    'DB_NAME'   => 'qwadmin', // 数据库名
+    'DB_USER'   => 'root', // 用户名
+    'DB_PWD'    => 'root', // 密码
+    'DB_PORT'   => 3306, // 端口
+    'DB_PREFIX' => 'qw_', // 数据库表前缀
+    'DB_CHARSET'=>  'utf8',      // 数据库编码默认采用utf8
+);
